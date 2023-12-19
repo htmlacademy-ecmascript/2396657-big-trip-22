619cc278e319425165ff6487deff787cf567a76b
Marina Menschikova <margelaevna@gmail.com>
2023-12-19 23:53:35 +0300

    Фикс

--- src/view/edit-view-form.js
+++ src/view/edit-view-form.js
@@ -2,7 +2,7 @@ import { createElement } from '../render.js';
 
 function createEditFormTemplate() {
   return (
-    `<form class="event event--edit" action="#" method="post">
+    `<li><form class="event event--edit" action="#" method="post">
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -154,7 +154,7 @@ function createEditFormTemplate() {
         <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
       </section>
     </section>
-  </form>`
+  </form></li>`
   );
 }
 
